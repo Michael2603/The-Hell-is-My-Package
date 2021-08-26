9c6023a66af8e7f72350846f1ec86a3b1a1dc39c
Michael2603 <michael.vergil@hotmail.com>
2021-08-26 09:37:36 -0300

    Guard improvements
    The guard can now shoot the player and can also start to search him.

--- Assets/Characters/Guards/Pistol/Scripts/Guard_PistolControll.cs
+++ Assets/Characters/Guards/Pistol/Scripts/Guard_PistolControll.cs
@@ -18,13 +18,29 @@ public class Guard_PistolControll : MonoBehaviour
 
     [SerializeField] GameObject bullet;
     [SerializeField] float bulletSpeed;
+    [SerializeField] Transform rotationTransform;
     [SerializeField] Transform bulletEmitter;
+    [SerializeField] float shootTimer;
+    float Timer;
+    bool canShoot = true;
+
+    public Transform target;
+    public float nextWayPoinDistance = 3f;
+    Path path;
+    int currentWayPoint = 0;
+    bool reachedEndOfPath = false;
+    Seeker seeker;
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         collider2d = GetComponent<Collider2D>();
         transform = GetComponent<Transform>();
+        Timer = shootTimer;
+    
+        coll = GetComponent<Collider2D>();
+        seeker = GetComponent<Seeker>();
+        InvokeRepeating("UpdatePath", 0, .5f);
     }
 
     void FixedUpdate()
@@ -37,6 +53,17 @@ public class Guard_PistolControll : MonoBehaviour
         else
         {
             Patrol();
+        }
+
+        if (!canShoot)
+        {
+            Timer -= Time.deltaTime;
+
+            if (Timer <= 0)
+            {
+                Timer = shootTimer;
+                canShoot = true;
+            }
         }
     }
 
@@ -80,17 +107,39 @@ public class Guard_PistolControll : MonoBehaviour
     {
         switch (persuitType)
         {
+            Collider2D playerFound = Physics2D.OverlapCircle(transform.position, 10, 1 << LayerMask.NameToLayer("Player"));
+            
             case "Focused":
-                Collider2D playerFound = Physics2D.OverlapCircle(transform.position, 10, 1 << LayerMask.NameToLayer("Player"));
-
                 if (playerFound)
                 {
-                    Shoot(playerFound.GetComponent<Transform>());
-                    Debug.Log("Piu piu");
+                    if (canShoot)
+                    {
+                        Shoot(playerFound.GetComponent<Transform>());
+                    }
+                }
+                else
+                {
+                    persuitType = "Searching";
+                    target = playerFound;
                 }
 
             break;
             case "Searching":
+                Collider2D playerSearch = Physics2D.OverlapCircle(transform.position, 15, 1 << LayerMask.NameToLayer("Player"));
+
+                //If the player is within an area, the guard starts to search for him and decreases the detectionMeter, else the guard stays still untill the detectionMeter is 0
+                if (playerSearch)
+                {
+                    GoAfterTarget();
+                    detectionRate -= Time.deltaTime * .4f;
+                    Collider2D playerFound = Physics2D.OverlapCircle(transform.position, 10, 1 << LayerMask.NameToLayer("Player"));
+                    if (playerFound)
+                        persuitType = "Focused";
+                }
+                else
+                {
+                    detectionRate -= Time.deltaTime * .1f;
+                }
 
             break;
         }
@@ -98,20 +147,53 @@ public class Guard_PistolControll : MonoBehaviour
 
     void Shoot(Transform player)
     {
-        // Retorna o plauer em angulo
-        float Angle = Mathf.Atan2(player.position.y, player.position.x) * Mathf.Rad2Deg ;
+        // Returns player's angle relative to guard's position
+         Vector3 relative = transform.InverseTransformPoint(player.position);
+        float Angle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg;
+        rotationTransform.rotation = Quaternion.Euler(Vector3.forward * Angle);
 
-        GameObject tempBullet = Instantiate(this.bullet, bulletEmitter.position, bullet.GetComponent<Transform>().rotation);
+        GameObject tempBullet = Instantiate(this.bullet, bulletEmitter.position, rotationTransform.rotation);
         Transform tempBulletTransform = tempBullet.GetComponent<Transform>();
-
-        //Adiciona uma rotação ao objeto dependendo da posição do player
-        tempBulletTransform.Rotate(0,0, Angle, Space.Self);
-
         tempBullet.GetComponent<Rigidbody2D>().AddForce(tempBulletTransform.right * bulletSpeed, ForceMode2D.Impulse);
+
+        canShoot = false;
     }
 
     void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(transform.position, 10);
+        Gizmos.DrawWireSphere(GetComponent<Transform>().position, 10);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+            path = p;
+            currentWayPoint = 0;
+    }
+
+    void GoAfterTarget()
+    {
+        if (path == null)
+            return;
+        
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ( (Vector2)path.vectorPath[currentWayPoint] - rigidbody2d.position ).normalized;
+        Vector2 force = direction * (moveSpeed) * Time.deltaTime;
+
+        rigidbody2d.AddForce(force);
+
+        float distance = Vector2.Distance(rigidbody2d.position, path.vectorPath[currentWayPoint]);
+
+        if (distance <= nextWayPoinDistance)
+            currentWayPoint++;
     }
 }
